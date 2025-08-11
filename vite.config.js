6d3fa01e322998fa2b2224eb71d696c3e77ee854
puremachine99 <puremachine99@gmail.com>
2025-08-11 11:56:43 +0700

    pull data dari vps ke local

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,17 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        tailwindcss(),
-    ],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    allowedHosts: ['dev.smartid.co.id'],
+  },
+
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js'],
+      refresh: true,
+    }),
+    tailwindcss(),
+  ],
 });
